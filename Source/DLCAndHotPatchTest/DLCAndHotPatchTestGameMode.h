dfe2298bf2a2c684029463f0f27556379d96078e
dorgon <dorgonman@hotmail.com>
2016-10-21 18:33:23 +0800

    init commit

---
+++ Source/DLCAndHotPatchTest/DLCAndHotPatchTestGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "DLCAndHotPatchTestGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DLCANDHOTPATCHTEST_API ADLCAndHotPatchTestGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
